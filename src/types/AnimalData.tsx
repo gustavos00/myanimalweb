abae3985f940e110e350df8a38ed311de20234aa
Gustavo Santos <z.gsantos20@gmail.com>
2022-03-10 23:40:54 -0100

    Adding types

---
+++ src/types/AnimalData.tsx
@@ -0,0 +1,21 @@
+import { VeterinarianData } from "./VeterinarianData";
+
+export interface AnimalData {
+  age: string;
+  birthday: string;
+  birthdayMonth: string;
+  breed: string;
+  idAnimal: number;
+  photoName: string;
+  photoUrl: string;
+  name: string;
+  trackNumber: string;
+  userIdUser: number;
+  veterinarianChatFingerprint: string,
+  userVeterinarian: number | null
+  userVeterinarianFk: VeterinarianData | null
+}
+
+export interface AnimalDataWithArraykey extends AnimalData {
+  arrayKey: number
+}
---
+++ src/types/UserData.tsx
@@ -0,0 +1,17 @@
+import { AnimalData } from "./AnimalData";
+import { UserAddressData } from "./UserAddressData";
+
+export interface UserData {
+  familyName: string;
+  givenName: string;
+  id?: number;
+  idUser?: number;
+  email: string;
+  phoneNumber: string;
+  token: string;
+  photoUrl: string;
+  isVeterinarian: boolean;
+  animalData: Array<AnimalData>
+  userAddress: UserAddressData;
+}
+

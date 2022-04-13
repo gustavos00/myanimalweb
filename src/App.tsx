55a7298e6875b44c0d75930885c4280eb6721b48
Gustavo Santos <z.gsantos20@gmail.com>
2022-04-13 02:21:56 +0000

    Applying the files context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import Global from "./assets/styles/global";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./contexts/user";
+import { StatesProvider } from "./contexts/states";
+import { FilesProvider } from "./contexts/files";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UpdateReport from "./pages/UpdateReport";
 
 function App() {
   return (
     <div>
       <UserProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
+        <FilesProvider>
+          <StatesProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/update" element={<UpdateReport />} />
+              </Routes>
+            </BrowserRouter>
+          </StatesProvider>
+        </FilesProvider>
       </UserProvider>
       <Global />
     </div>

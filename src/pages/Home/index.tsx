e4f2094545b316a1a99aa0d9c32c04528fdcd04f
Gustavo Santos <z.gsantos20@gmail.com>
2022-04-13 02:17:33 +0000

    Conclude home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,79 +3,84 @@ import Filter from "../../components/Filter";
 import Loading from "../../components/Loading";
 import CreateAddress from "../../components/CreateAddress";
 import StatesContext from "../../contexts/states";
-import api from "../../api/api";
 import UserContext from "../../contexts/user";
+import Table from "../../components/Table";
 
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 
 import * as S from "./styles";
 
-import { useLocation } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
-import { Tr } from "react-super-responsive-table";
-import { AnimalData } from "../../types/AnimalData";
-
-interface IPageProps {
-  haveAddress: boolean;
-}
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { formatDate } from "../../utils/date";
 
 function Home() {
-  const state = useLocation().state as IPageProps;
+  const navigate = useNavigate();
 
   const { isLoading } = useContext(StatesContext);
-  const { user } = useContext(UserContext);
+  const { user, animals, events } =
+    useContext(UserContext);
 
-  const [haveAddress, setHaveAddress] = useState<boolean>(state.haveAddress);
-  const [animals, setAnimals] = useState<Array<AnimalData>>([]);
+  const [haveAddress, setHaveAddress] = useState<boolean>(
+    user?.haveAddress ? user.haveAddress : false
+  );
 
-  useEffect(() => {
-    const handleVeterinarianAnimalsRequest = async () => {
-      const response = await api.get(
-        `veterinarian/getAnimals?id=${user?.idUser}`
-      );
-      setAnimals(response.data);
-    };
-    handleVeterinarianAnimalsRequest();
-  }, []);
+  const registeredAnimalsTableHeaders = [
+    "Name",
+    "Age",
+    "Breed",
+    "Track Number",
+    "Birthday",
+    "Birthday Month",
+    "Owner Name",
+    "Owner Contact",
+  ];
+  const idkYet = ["Report", "Status", "Type", "Animal Name", "Date"];
+
   return (
     <>
       <S.Container>
         <Sidebar />
         <S.Content>
-          <S.TableContainer>
-            <S.STable>
-              <S.STHead>
-                <Tr>
-                  <S.STh>Name</S.STh>
-                  <S.STh>Age</S.STh>
-                  <S.STh>Breed</S.STh>
-                  <S.STh>Track Number</S.STh>
-                  <S.STh>Birthday</S.STh>
-                  <S.STh>Birthday Month</S.STh>
-                  <S.STh>Owner Name</S.STh>
-                  <S.STh>Owner Contact</S.STh>
-                </Tr>
-              </S.STHead>
-              <S.STBody>
-                <>
-                  {animals.map((element) => {
-                    return (
-                      <Tr key={element.idAnimal}>
-                        <S.STd>{element.name}</S.STd>
-                        <S.STd>{element.age}</S.STd>
-                        <S.STd>{element.breed}</S.STd>
-                        <S.STd>{element.trackNumber}</S.STd>
-                        <S.STd>{element.birthday}</S.STd>
-                        <S.STd>{element.birthday}</S.STd>
-                        <S.STd>{element.user.givenName} {element.user.familyName}</S.STd>
-                        <S.STd>{element.user.email}</S.STd>
-                      </Tr>
-                    );
-                  })}
-                </>
-              </S.STBody>
-            </S.STable>
-          </S.TableContainer>
+          <Table
+            title={"Registered Animals"}
+            header={registeredAnimalsTableHeaders}
+          >
+            <>
+              {animals?.map((element, index) => (
+                <S.STr iscolored={index % 2 === 0} key={index}>
+                  <S.STd>{element.name}</S.STd>
+                  <S.STd>{element.age}</S.STd>
+                  <S.STd>{element.breed}</S.STd>
+                  <S.STd>{element.trackNumber}</S.STd>
+                  <S.STd>{element.birthday}</S.STd>
+                  <S.STd>{element.birthday}</S.STd>
+                  <S.STd>
+                    {element.user.givenName} {element.user.familyName}
+                  </S.STd>
+                  <S.STd>{element.user.email}</S.STd>
+                </S.STr>
+              ))}
+            </>
+          </Table>
+
+          <Table title={"Registered Events"} header={idkYet}>
+            <>
+              {events?.map((element, index) => (
+                <S.STr
+                  onClick={() => navigate("/update", { state: element })}
+                  iscolored={index % 2 === 0}
+                  key={index}
+                >
+                  <S.STd>{element.report}</S.STd>
+                  <S.STd>{element.eventsStatus.value}</S.STd>
+                  <S.STd>{element.eventsType.value}</S.STd>
+                  <S.STd>{element.animal.name}</S.STd>
+                  <S.STd>{formatDate(element.createdAt)}</S.STd>
+                </S.STr>
+              ))}
+            </>
+          </Table>
         </S.Content>
       </S.Container>
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
-import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import { Tr, Td } from "react-super-responsive-table";
 
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100%;
+  padding-bottom: 100px;
+  
   display: flex;
   flex-direction: row;
 `;
@@ -13,53 +15,23 @@ export const Content = styled.div`
   padding-top: 100px;
 
   display: flex;
-  justify-content: center;
-  align-items: flex-start;
-`;
-
-export const TableContainer = styled.div`
-  width: 60vw;
-
-  display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
-`;
-
-export const STable = styled(Table)`
-  border-spacing: 0;
-`;
-
-export const STHead = styled(Thead)`
-  background: #d66d42;
-  color: #fff;
-
-  Th {
-    &:first-child {
-      border-top-left-radius: 5px;
-    }
-
-    &:last-child {
-      border-top-right-radius: 5px;
-    }
-  }
-`;
-
-export const STBody = styled(Tbody)`
-  cursor: pointer;
-
-  Tr {
-    background-color: rgba(241, 156, 121, 0.3);
-    &:hover {
-      background-color: rgba(241, 156, 121, 0.6);
-    }
-  }
-`;
-
-export const STh = styled(Th)`
-  text-align: left;
-  padding: 0.6rem;
 `;
 
 export const STd = styled(Td)`
   padding: 5px;
+
+  font-size: 14px;
+  font-weight: normal;
+`;
+
+export const STr = styled(Tr)<{ iscolored: boolean }>`
+  padding: 5px;
+
+  font-size: 14px;
+  font-weight: normal;
+
+  background: ${(props) => (props.iscolored ? "rgba(255,255,255,.3)" : "rgba(255,255,255,.1)")};
 `;
